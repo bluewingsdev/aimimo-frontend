63206cb2f955f7293e690f6c1815d06da9f58eab
BlueWings2017 <bw2017@blws.co.kr>
2023-11-30 10:59:29 +0900

    로컬, 빌드 환경변수 설정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+require("dotenv").config();
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
@@ -6,12 +8,11 @@ module.exports = defineConfig({
     port: 8081,
     proxy: {
       '/api': { 
-        target: 'http://localhost:8080',
+        target: process.env.BASE_URL,
         changeOrigin: true,
       }, 
     },
   },
-
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
