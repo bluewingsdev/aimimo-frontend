9348be94d89c96d7b12393604bf2805db92c0460
gusalnim <you@example.com>
2023-12-06 08:12:02 +0900

    color set

--- src/components/common/AppSettings.vue
+++ src/components/common/AppSettings.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="settings-wrap">
         <!-- 메뉴 위치 : left-menu-wrap, header-wrap : 각각 active -->
-        <!-- 셋팅 영역 : left-menu-wrap active : tab-area 안에 settings-wrap -->
-        <!-- 셋팅 영역 : header-wrap active : header-wrap 안에 settings-wrap -->
+        <!-- 셋팅 영역 : left-menu-wrap active : open-page-wrap 안에 <AppSettings/> -->
+        <!-- 셋팅 영역 : header-wrap active : header-wrap 안에 <AppSettings/> -->
         <div class="">텝닫기</div>
         <div class="">새로고침</div>
         <div class="">
