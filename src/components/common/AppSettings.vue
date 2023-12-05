7d1ed2c3753f8be4e5f45a7b424d711dd1289e9a
gusalnim <you@example.com>
2023-12-06 08:16:01 +0900

    setting

--- src/components/common/AppSettings.vue
+++ src/components/common/AppSettings.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="settings-wrap">
-        <!-- 메뉴 위치 : left-menu-wrap, header-wrap : 각각 active -->
+        <!-- 메뉴 위치 : left-menu-wrap, header-wrap : 선택된 메뉴의 클래스에 active -->
         <!-- 셋팅 영역 : left-menu-wrap active : open-page-wrap 안에 <AppSettings/> -->
         <!-- 셋팅 영역 : header-wrap active : header-wrap 안에 <AppSettings/> -->
         <div class="">텝닫기</div>
@@ -10,7 +10,7 @@
             <div class="settings">
                 <div class="tit">색상모드</div>
                 <ul class="color-set">
-                    <li class="navy"><span class=""></span>Navy</li>
+                    <li class="navy active"><span class=""></span>Navy</li>
                     <li class="gray"><span class=""></span>Gray</li>
                     <li class="orange"><span class=""></span>Orange</li>
                     <li class="blue"><span class=""></span>Blue</li>
