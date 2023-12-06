d2b46dcaa71b79d5f75d765f9764c6c244b73374
gusalnima <gusalnim@gmail.com>
2023-12-06 23:59:43 +0900

    menu

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,72 +1,46 @@
 <template>
     <div class="header-wrap" :class="{ active: isTopMenu }" >
-        <!-- one-depth에 mouse over하면 header에 active -->
         <div class="header">
-             <h1 class="logo"><img :src="require(`@/assets/images/common/logo.png`)" alt=""></h1>
+            <h1 class="logo"><img :src="require(`@/assets/images/common/logo.png`)" alt=""></h1>
             <ul class="one-depth">
-                <!-- one 기본 첫번째는 active, 선택했을때 active -->
-                <li class="one active">
-                    <IconSetting 
-                    />
-                    <span>ONE DEPTH</span>
-                    <ul class="two-depth">
-                        <!-- two 선택했을때 active -->
-                        <li class="two active">
-                            <div class="boxs">
-                                <span class="">TWO DEPTH</span>
-                                <!-- three depth가 있을때 arr -->
-                                <span class="arr">&nbsp;</span>
-                            </div>
-                            <ul class="three-depth">
-                                <!-- three 클릭했을때 active -->
-                                <li class="three active">
-                                    <span>THREE DEPTH</span>
+                <template v-for="(depth1) in menuList.filter(x => x.upMenuId == null)" :key="depth1">
+                    <!-- 현재 상태에서.. 
+                        one 를 오버하면..
+                        one-depth 에 addClass "hover" 하고,
+                        아웃하면 removeClass "hover" 해주세요.
+                    -->
+                    <li class="one" @mouseover="onMenuMouseover(depth1)" >
+                        <IconSetting 
+                            :isActive="depth1.isActive"
+                        />
+                        <span>{{ depth1.menuNm }}</span>
+                        <ul class="two-depth" v-if="menuList.filter(x => x.upMenuId == depth1.menuId).length > 0">
+                            <template v-for="(depth2) in menuList.filter(x => x.upMenuId == depth1.menuId)" :key="depth2">
+                                <li class="two" :class="{ active: depth2.isActive }">
+                                    <div v-if="!depth2.url" class="boxs" @click="onMenuClick(depth2)">
+                                        <span class="">{{ depth2.menuNm }}</span>
+                                        <span v-if="menuList.filter(x => x.upMenuId == depth2.menuId).length > 0" class="arr">&nbsp;</span>
+                                    </div>
+                                    <router-link v-else class="boxs" :to="depth2.url">
+                                        <span class="">{{ depth2.menuNm }}</span>
+                                    </router-link>
+                                    <ul class="three-depth" v-if="!depth2.url && menuList.filter(x => x.upMenuId == depth2.menuId).length > 0">
+                                        <template v-for="(depth3) in menuList.filter(x => x.upMenuId == depth2.menuId)" :key="depth3">
+                                            <router-link v-if="depth3.url" :to="depth3.url">
+                                                <li class="three">
+                                                    <span>{{ depth3.menuNm }}</span>
+                                                </li>
+                                            </router-link>
+                                            <li v-else class="three">
+                                                <span>{{ depth3.menuNm }}</span>
+                                            </li>
+                                        </template>
+                                    </ul>
                                 </li>
-                                <li class="three">
-                                    <span>THREE DEPTH</span>
-                                </li>
-                                <li class="three">
-                                    <span>THREE DEPTH</span>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="two">
-                            <div class="boxs">
-                                <span class="">TWO DEPTH</span>
-                                <span class="arr">&nbsp;</span>
-                            </div>
-                            <ul class="three-depth">
-                                <li class="three">
-                                    <span>THREE DEPTH</span>
-                                </li>
-                                <li class="three">
-                                    <span>THREE DEPTH</span>
-                                </li>
-                                <li class="three">
-                                    <span>THREE DEPTH</span>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="two active">
-                            <div class="boxs">
-                                <span class="">TWO DEPTH</span>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
-                <li class="one">
-                    <img src="../assets/images/common/menu-setting.svg" alt="" />
-                    <span>ONE DEPTH</span>
-                    <ul class="two-depth">
-                        <li class="two">
-                            <span>TWO DEPTH</span>
-                        </li>
-                    </ul>
-                </li>
-                <li class="one">
-                    <img src="../assets/images/common/menu-setting.svg" alt="" />
-                    <span>ONE DEPTH</span>
-                </li>
+                            </template>
+                        </ul>
+                    </li>
+                </template>
             </ul>
             <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
@@ -91,17 +65,115 @@
     import AppSettings from "@/components/common/AppSettings.vue";
     export default {
         name: "AppHeader",
-        components: {
-            AppSettings,IconSetting
+        components: {IconSetting,AppSettings},
+        data() {
+            return {
+                menuList: [
+                    {
+                        menuId: 'MENU 1',
+                        menuNm: 'MENU 1',
+                        isActive: true,
+                        upMenuId: null,
+                        url: null
+                    },
+                    {
+                        menuId: 'MENU 1-1',
+                        menuNm: 'MENU 1-1',
+                        isActive: false,
+                        upMenuId: 'MENU 1',
+                        url: null
+                    },
+                    {
+                        menuId: 'MENU 1-2',
+                        menuNm: 'MENU 1-2',
+                        isActive: false,
+                        upMenuId: 'MENU 1',
+                        url: null
+                    },
+                    {
+                        menuId: 'MENU 1-3',
+                        menuNm: 'MENU 1-3',
+                        isActive: false,
+                        upMenuId: 'MENU 1',
+                        url: null
+                    },
+                    {
+                        menuId: 'MENU 1-1-1',
+                        menuNm: '메인',
+                        isActive: false,
+                        upMenuId: 'MENU 1-1',
+                        url: '/'
+                    },
+                    {
+                        menuId: 'MENU 1-1-2',
+                        menuNm: '회원가입',
+                        isActive: false,
+                        upMenuId: 'MENU 1-1',
+                        url: '/register'
+                    },
+                    {
+                        menuId: 'MENU 1-1-3',
+                        menuNm: '로그인',
+                        isActive: false,
+                        upMenuId: 'MENU 1-1',
+                        url: '/login'
+                    },
+                    {
+                        menuId: 'MENU 2',
+                        menuNm: 'MENU 2',
+                        isActive: true,
+                        upMenuId: null,
+                        url: null
+                    },
+                    {
+                        menuId: 'MENU 2-1',
+                        menuNm: 'MENU 2-1',
+                        isActive: false,
+                        upMenuId: 'MENU 2',
+                        url: null
+                    },
+                    {
+                        menuId: 'MENU 2-2',
+                        menuNm: 'MENU 2-2',
+                        isActive: false,
+                        upMenuId: 'MENU 2',
+                        url: null
+                    },
+                    {
+                        menuId: 'MENU 3',
+                        menuNm: 'MENU 3',
+                        isActive: true,
+                        upMenuId: null,
+                        url: null
+                    }
+                ]
+            }
+            
         },
-        data() {},
         computed: {
             isTopMenu() {
                 return this.$store.state.common.isTopMenu
             },
         },
         created() {},
-        methods: {},
+        methods: {
+            onMenuClick(menu) {
+                if(menu.upMenuId == null) {
+                    if(!menu.isActive) {
+                        this.menuList.filter(x => x.upMenuId == null && x.menuId !== menu.menuId).forEach(x => x.isActive = false);
+                    }
+                }
+                
+                this.menuList.find(x => x.menuId === menu.menuId).isActive = !menu.isActive;
+            },
+            onMenuMouseover(menu) {
+                this.menuList.filter(x => x.upMenuId == null && x.menuId !== menu.menuId).forEach(x => x.isActive = false);
+                this.menuList.find(x => x.menuId === menu.menuId).isActive = true;
+            },
+            movePage() {
+
+            }
+        }
     }
 </script>
   
