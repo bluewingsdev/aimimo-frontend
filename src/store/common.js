79f8dffe1e1cd968550848151ccbb8ee237c3f49
gusalnima <gusalnim@gmail.com>
2023-12-06 23:11:43 +0900

    menu

--- src/store/common.js
+++ src/store/common.js
@@ -1,25 +1,25 @@
 const initialState = {
-  isTopMenu: false,
-  themeColor: 'navy'
-};
+  isTopMenu: true,
+  themeColor: "navy",
+}
 
 export const common = {
   namespaced: true,
   state: initialState,
   actions: {
     toggleTopMenu({ commit }) {
-      commit('toggleTopMenu');
+      commit("toggleTopMenu")
     },
     setThemeColor({ commit }, value) {
-      commit('setThemeColor', value);
-    }
+      commit("setThemeColor", value)
+    },
   },
   mutations: {
     toggleTopMenu(state) {
-      state.isTopMenu = !state.isTopMenu;
+      state.isTopMenu = !state.isTopMenu
     },
     setThemeColor(state, value) {
-      state.themeColor = value;
-    }
-  }
-};
+      state.themeColor = value
+    },
+  },
+}
